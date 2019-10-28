5f3c00f501606d388671025a3bb6d628dafb3b61
kelly r maass <kmaass77627@gmail.com>
2019-10-28 17:59:51 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,33 @@
+class Dog 
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def breed(breed)
+    @breed = breed 
+  end 
+  
+  def breed 
+    @breed
+  end 
+
+
+def initialize(breed)
+  @breed = breed 
+end 
+
+def breed = (breed)
+  @breed = breed
+end 
+
+def breed
+  @breed
+end 
+end 
+
+
+
+
+
+end 
